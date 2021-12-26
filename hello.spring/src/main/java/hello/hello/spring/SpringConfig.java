ef08ebb17982b64773e9e088c5644a43993412cf
yukuda <leeyookoo@naver.com>
2021-12-26 17:08:03 +0900

    SpringConfig

---
+++ hello.spring/src/main/java/hello/hello/spring/SpringConfig.java
@@ -0,0 +1,30 @@
+package hello.hello.spring;
+
+import hello.hello.spring.repository.JdbcMemberRepository;
+import hello.hello.spring.repository.MemberRepository;
+import hello.hello.spring.repository.MemoryMemberRepository;
+import hello.hello.spring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+    }
+}
