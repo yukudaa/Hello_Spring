83566dd67cd92c9fb11f2ef0da3a7d9eb796c5d2
yukuda <leeyookoo@naver.com>
2021-12-26 17:07:23 +0900

    MemberController

--- hello.spring/src/main/java/hello/hello/spring/controller/MemberController.java
+++ hello.spring/src/main/java/hello/hello/spring/controller/MemberController.java
@@ -1,17 +1,44 @@
 package hello.hello.spring.controller;
 
+import hello.hello.spring.domain.Member;
 import hello.hello.spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@Controller
+import java.util.List;
+//@Controller
 public class MemberController {
 
     private final MemberService memberService;
 
-    @Autowired      //controller에 service를 의존해줌(뭔가 넣어준 느낌)
+//    @Autowired      //controller에 service를 의존해줌(뭔가 넣어준 느낌)
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
+
     }
 
 }
